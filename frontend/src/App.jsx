4b02cb0cb6f99b85e9b1634a52443ab92114fd1c
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-02-11 21:17:50 +0530

    add files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,30 @@
-import { Route } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+import ExplorePage from "./pages/ExplorePage";
+import LikesPage from "./pages/LikesPage";
+
+import Sidebar from "./components/Sidebar";
 
 
 function App() {
-
-
   return (
     <div className="flex">
-      <sidebar>
-        <div>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/explore" element={<ExplorePage />} />
-            <Route path="/likes" element={<LikesPage />} />
-
-
-          </Routes>
+      <Sidebar />
+        <div className="max-w-5xl my-5 text-white mx-auto transition-all">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/likes" element={<LikesPage />} />
+        </Routes>
         </div>
-      </sidebar>
-
-     
+    
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
