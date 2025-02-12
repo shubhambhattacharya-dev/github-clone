9715bd17ca839166f9759ba2cd4818b4fb0c1631
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-02-12 10:10:42 +0530

    sidebar setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 
-import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
 import HomePage from "./pages/HomePage";
 import ExplorePage from "./pages/ExplorePage";
 import LikesPage from "./pages/LikesPage";
 
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./component/Sidebar";
 
 
 function App() {
   return (
-    <div className="flex">
+    <div className="flex text-white">
       <Sidebar />
-        <div className="max-w-5xl my-5 text-white mx-auto transition-all">
+        <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
@@ -21,6 +21,7 @@ function App() {
           <Route path="/explore" element={<ExplorePage />} />
           <Route path="/likes" element={<LikesPage />} />
         </Routes>
+     
         </div>
     
     </div>
