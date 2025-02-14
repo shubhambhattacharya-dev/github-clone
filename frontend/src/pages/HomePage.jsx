57a5595627b91f0eb8c37eec576cd19f84e62599
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-02-14 18:42:02 +0530

    now setup for homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Search from '../component/Search'; // Corrected path
+import SortRepos from '../components/SortRepos';
+import ProfileInfo from '../components/ProfileInfo';
+import Repos from '../components/Repos';
+import Spinner from '../components/Spinner';
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+    return (
+        <div className='m-4'>
+            <Search />
+            <SortRepos />
+            <div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
+                <ProfileInfo />
+                <Repos />
+                <Spinner />
+            </div>
+        </div>
+    );
+};
 
-export default HomePage
+export default HomePage;
