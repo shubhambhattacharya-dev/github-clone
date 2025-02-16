bb88820b03e223a73739c6c1ad763f7a2ccf5a12
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-02-16 14:33:13 +0530

    profiles set up

--- frontend/src/component/Repos.jsx
+++ frontend/src/component/Repos.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Repos = () => {
   return (
-    <div>Repos</div>
+    <div className={`lg:w-2/3 w-full bg-glass rounded-lg px-8 py-6`}>
+			<ol className='relative border-s border-gray-200'>
+				<Repo />
+				<Repo />
+				<Repo />
+			</ol>
+		</div>
   )
 }
 
