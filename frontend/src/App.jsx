b22390c26fcdf2bccedb9b7d65a882d6eb540b2a
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-02-17 10:44:08 +0530

    corrct code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import LoginPage from "./pages/LoginPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
@@ -22,6 +23,7 @@ function App() {
           <Route path="/explore" element={<ExplorePage />} />
           <Route path="/likes" element={<LikesPage />} />
         </Routes>
+        <Toaster/>
      
         </div>
     
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,23 +1,50 @@
+import React, { useEffect, useState, useCallback } from "react";
+import toast from "react-hot-toast";
 import ProfileInfo from "../component/ProfileInfo.jsx";
 import Repos from "../component/Repos.jsx";
 import Search from "../component/Search.jsx";
 import SortRepos from "../component/SortRepos.jsx";
 import Spinner from "../component/Spinner.jsx";
 
-
 const HomePage = () => {
-    return (
-        <div className='m-4'>
-            <Search />
-            <SortRepos />
-            <div className='flex gap-4 flex-col lg:flex-row justify-center items-start overflow-hidden'>
-                <ProfileInfo />
-                <Repos />
-                <Spinner />
+  const [userProfile, setUserProfileInfo] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-            </div>
-        </div>
-    );
+  const getUserProfileAndRepos = useCallback(async () => {
+    setLoading(true);
+    try {
+      const userRes = await fetch(
+        "https://api.github.com/users/shubhambhattacharya-dev"
+      );
+      const profileData = await userRes.json();
+      setUserProfileInfo(profileData);
+
+      const repoRes = await fetch(profileData.repos_url);
+      const repoData = await repoRes.json();
+      setRepos(repoData);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getUserProfileAndRepos();
+  }, [getUserProfileAndRepos]);
+
+  return (
+    <div className="m-4">
+      <Search />
+      <SortRepos />
+      <div className="flex gap-4 flex-col lg:flex-row justify-center items-start overflow-hidden">
+        <ProfileInfo userProfile={userProfile} />
+        <Repos repos={repos} />
+        {loading && <Spinner />}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
