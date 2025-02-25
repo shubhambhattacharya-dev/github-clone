3468b143bc2289d1a69626b54a688327646d40ad
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-02-25 17:17:20 +0530

    set up for search bar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,49 +11,56 @@ const HomePage = () => {
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const getUserProfileAndRepos = useCallback(async (username="shubhambhattacharya-dev") => {
-    setLoading(true);
-    try {
-      const userRes = await fetch(
-        `https://api.github.com/users/${username}`
-      );
-      const profileData = await userRes.json();
-      setUserProfileInfo(profileData);
+  const getUserProfileAndRepos = useCallback(
+    async (username = "shubhambhattacharya-dev") => {
+      setLoading(true);
+      try {
+        const userRes = await fetch(
+          `https://api.github.com/users/${username}`
+        );
+        const profileData = await userRes.json();
+        setUserProfileInfo(profileData);
 
-      const repoRes = await fetch(profileData.repos_url);
-      const repoData = await repoRes.json();
-      setRepos(repoData);
-    } catch (error) {
-      toast.error(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+        const repoRes = await fetch(profileData.repos_url);
+        const repoData = await repoRes.json();
+        setRepos(repoData);
+        console.log("userProfile:", profileData);
+        console.log("repos:", repoData);
+        return { userProfile: profileData, repos: repoData };
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    []
+  );
 
   useEffect(() => {
     getUserProfileAndRepos();
   }, [getUserProfileAndRepos]);
 
-  const onSearch = async (e,username) => {
+  const onSearch = async (e, username) => {
     e.preventDefault();
 
     setLoading(true);
     setRepos([]);
     setUserProfileInfo(null);
 
-      await getUserProfileAndRepos(username);
+    const { userProfile, repos } = await getUserProfileAndRepos(username);
 
-  }
-   
+    setUserProfileInfo(userProfile);
+    setRepos(repos);
+    setLoading(false);
+  };
 
   return (
     <div className="m-4">
-      <Search />
+      <Search onSearch={onSearch} />
       <SortRepos />
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start overflow-hidden">
         {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
         {repos.length > 0 && !loading && <Repos repos={repos} />}
-       
         {loading && <Spinner />}
       </div>
     </div>
