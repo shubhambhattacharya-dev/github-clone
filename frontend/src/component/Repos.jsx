d44477bd25365a6e8d8d78ffe56a479b4ecdf89c
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-02-28 18:20:36 +0530

    expor fix

--- frontend/src/component/Repos.jsx
+++ frontend/src/component/Repos.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Repo from "./Repo";
 
-const Repos = ({ repos }) => {
+const Repos = ({ repos, alwaysFullWidth = false }) => {
+  const containerClass = alwaysFullWidth ? "w-full" : "lg:w-2/3 w-full";
+
   return (
-    <div className="lg:w-2/3 w-full bg-glass rounded-lg px-8 py-6">
-      <ol className="relative border-s border-gray-200">
-        {repos.map(repo => (
-          <Repo key={repo.id} repo={repo} />
-        ))}
-        {repos.length === 0 && (
-          <p className="flex items-center justify-center h-32">No repos found</p>
-        )}
-      </ol>
+    <div className={`${containerClass} bg-glass rounded-lg px-8 py-6`}>
+      {repos.length > 0 ? (
+        <ol className="relative border-s border-gray-200">
+          {repos.map((repo) => (
+            <Repo key={repo.id} repo={repo} />
+          ))}
+        </ol>
+      ) : (
+        <p className="flex items-center justify-center h-32">
+          No repos found
+        </p>
+      )}
     </div>
   );
 };
