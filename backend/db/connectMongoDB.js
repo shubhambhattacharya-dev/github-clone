ee3bbd60e766c5c927d3d7f7a7488dc3c044535c
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-04-12 14:14:10 +0530

    fix mongoodb

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
 export default async function connectMongoDB() {
-    try {
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("✅ MongoDB Connected Successfully!");
-    } catch (error) {
-        console.error("❌ Error connecting to MongoDB:", error.message);
-        process.exit(1); // Exit process if database connection fails
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+
+    console.log("✅ MongoDB Connected Successfully!");
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
 }
