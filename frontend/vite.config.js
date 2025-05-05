1f5e0f696cb638462bc2ba132267ed2a60e96165
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-05-05 15:37:33 +0530

    remove nested git

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,23 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
+  // Add only these new configuration lines
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          reactrouter: ["react-router", "react-router-dom"],
+        },
+      },
+    },
+  },
 });
