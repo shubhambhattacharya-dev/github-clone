d05a05bb49205873728b04935c61967c73bdead3
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-05-17 10:22:04 +0530

    typo error

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,47 +1,46 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        name: {
-            type: String,
-            default: "",
-        },
-        profileUrl: {
-            type: String,
-            required: true,
-        },
-        avatarUrl: {
-            type: String,
-        },
-        likedprofiles: {
-            type: [String],
-            default: [],
-        },
-        likedBY: [
-            {
-                userName: {
-                    type: String,
-                    required: true,
-                },
-                avatarUrl: {
-                    type: String,
-                },
-                likedDate: {
-                    type: Date,
-                    default: Date.now,
-                },
-            },
-        ],
-    },
-    { timestamps: true }
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		name: {
+			type: String,
+			default: "", //
+		},
+		profileUrl: {
+			type: String,
+			required: true,
+		},
+		avatarUrl: {
+			type: String,
+		},
+		likedProfiles: {
+			type: [String],
+			default: [],
+		},
+		likedBy: [
+			{
+				username: {
+					type: String,
+					required: true,
+				},
+				avatarUrl: {
+					type: String,
+				},
+				likedDate: {
+					type: Date,
+					default: Date.now,
+				},
+			},
+		],
+	},
+	{ timestamps: true }
 );
 
-const user = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default user;
-
+export default User;
