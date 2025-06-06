816d36a269ea906c60a1e62f928c849cea3f7100
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-06-06 10:57:49 +0530

    undo some changes

--- frontend/src/component/Sidebar.jsx
+++ frontend/src/component/Sidebar.jsx
@@ -49,7 +49,7 @@ const Sidebar = () => {
             loading="lazy"
             width="32"
             height="32"
-            link="https://github.com"
+            
           />
         </Link>
 
